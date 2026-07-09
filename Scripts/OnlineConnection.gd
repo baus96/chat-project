1b02a3426a4b767bb1309b19beb1a3d59946a5f4
Fahri Aşıt <bademmfa96@gmail.com>
2026-07-09 19:06:31 +0300

    Update OnlineConnection.gd

--- Scripts/OnlineConnection.gd
+++ Scripts/OnlineConnection.gd
@@ -3,3 +3,13 @@ extends Node
 const PORT := 7777
 
 var peer = ENetMultiplayerPeer.new()
+
+func host_server():
+	peer.create_server(PORT)
+	multiplayer.multiplayer_peer = peer
+	print("Server Created, PORT:" + str(PORT))
+	
+func join_server(ip: = "localhost", port: = PORT):
+	peer.create_client(ip, port)
+	multiplayer.multiplayer_peer = peer
+	

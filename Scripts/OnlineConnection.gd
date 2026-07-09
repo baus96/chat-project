5ca7c543a2b68f33e942dd9201e7d2323f048e02
Fahri Aşıt <bademmfa96@gmail.com>
2026-07-09 19:19:42 +0300

    update network

--- Scripts/OnlineConnection.gd
+++ Scripts/OnlineConnection.gd
@@ -4,12 +4,58 @@ const PORT := 7777
 
 var peer = ENetMultiplayerPeer.new()
 
+func _ready() -> void:
+	# --- HER İKİ TARAFTA DA (Server & Client) ÇALIŞAN SİNYALLER ---
+	# Yeni bir oyuncu bağlandığında tetiklenir (Herkes duyar)
+	multiplayer.peer_connected.connect(_on_peer_connected)
+	# Bir oyuncunun bağlantısı koptuğunda tetiklenir (Herkes duyar)
+	multiplayer.peer_disconnected.connect(_on_peer_disconnected)
+
+	# --- SADECE CLIENT (İSTEMCİ) TARAFINDA ÇALIŞAN SİNYALLER ---
+	# Sunucuya başarıyla bağlandığımızda tetiklenir
+	multiplayer.connected_to_server.connect(_on_connected_to_server)
+	# Sunucuya bağlanma denemesi başarısız olduğunda tetiklenir
+	multiplayer.connection_failed.connect(_on_connection_failed)
+	# Sunucu kapandığında veya bizi attığında tetiklenir
+	multiplayer.server_disconnected.connect(_on_server_disconnected)
+
+
 func host_server():
 	peer.create_server(PORT)
 	multiplayer.multiplayer_peer = peer
 	print("Server Created, PORT:" + str(PORT))
 	
-func join_server(ip: = "localhost", port: = PORT):
+func join_server(ip: = "127.0.0.1", port: = PORT):
 	peer.create_client(ip, port)
 	multiplayer.multiplayer_peer = peer
+	print("joined server")
+	
+	
+	# [Server & Client] Herhangi bir oyuncu bağlandığında
+func _on_peer_connected(id: int):
+	print("Yeni bir oyuncu ağa katıldı! Benzersiz ID: ", id)
+	
+	
+# [Server & Client] Herhangi bir oyuncu ayrıldığında
+func _on_peer_disconnected(id: int):
+	print("Bir oyuncu ağdan ayrıldı. Benzersiz ID: ", id)
+	
+	
+	
+	
+# [Sadece Client] Biz sunucuya başarıyla bağlandığımızda
+func _on_connected_to_server():
+	print("BAŞARILI: Sunucuya bağlandım! Benim ID'm: ", multiplayer.get_unique_id())
+
+# [Sadece Client] Biz sunucuya bağlanmayı başaramadığımızda
+func _on_connection_failed():
+	print("HATA: Sunucuya bağlanılamadı. Sunucu kapalı olabilir veya IP/Port yanlıştır.")
+	
+
+# [Sadece Client] Sunucu çöktüğünde veya bizi disconnet ettiğinde
+func _on_server_disconnected():
+	print("BİLGİ: Sunucu kapandı veya bağlantınız kesildi!")
+	
+
+
 	
